250aecfcdf52ac4bc7fbc1bd5028ed237d4d4879
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2020-08-14 10:10:42 +0530

    Updates

---
+++ precache-manifest.c2c8b47234753e1799a5da04e3112066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c775116d3dcc71e640753ac2e22b52c",
+    "url": "/facebook-messenger-clone/index.html"
+  },
+  {
+    "revision": "14a64af914b484445fd6",
+    "url": "/facebook-messenger-clone/static/css/main.1f288623.chunk.css"
+  },
+  {
+    "revision": "2d83ace4ba05bd211857",
+    "url": "/facebook-messenger-clone/static/js/2.096b59c9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-messenger-clone/static/js/2.096b59c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a64af914b484445fd6",
+    "url": "/facebook-messenger-clone/static/js/main.aa6414f2.chunk.js"
+  },
+  {
+    "revision": "67d65d09f6d69bea4086",
+    "url": "/facebook-messenger-clone/static/js/runtime-main.f5931f04.js"
+  }
+]);
